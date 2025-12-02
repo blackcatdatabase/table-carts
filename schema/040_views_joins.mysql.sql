16b2b29604ac3b0993dc5fcdac866e37eac97bd3
Vito416 <vito416@protonmail.com>
2025-12-02 19:25:55 +0100

    Sync submodule state after YAML views + upsert/coverage fixes

--- schema/040_views_joins.mysql.sql
+++ schema/040_views_joins.mysql.sql
@@ -1,7 +1,7 @@
--- Auto-generated from joins-mysql.psd1 (map@mtime:2025-11-27T17:49:37Z)
+-- Auto-generated from joins-mysql.yaml (map@94ebe6c)
 -- engine: mysql
 -- view:   carts_with_items
--- Carts with item count and subtotal snapshot
+
 CREATE OR REPLACE ALGORITHM=TEMPTABLE SQL SECURITY INVOKER VIEW vw_carts_with_items AS
 SELECT
   c.id,
